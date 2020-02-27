4b3b63384333d4bfce1d6a693da72e0716389424
samreid <reids@colorado.edu>
2020-02-27 12:08:01 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/smiley-man-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import smileyMan from './smileyMan.js';
+
+const smileyManStrings = getStringModule( 'SMILEY_MAN' );
+
+smileyMan.register( 'smileyManStrings', smileyManStrings );
+
+export default smileyManStrings;
